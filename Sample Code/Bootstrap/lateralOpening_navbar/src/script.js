f625905ee9f24727e341bffebfc40e52890c92c7
zchen125 <zchen125@stevens.edu>
2022-09-30 13:40:27 -0400

    some basic layouts added

--- Sample Code/Bootstrap/lateralOpening_navbar/src/script.js
+++
@@ -1,7 +0,0 @@
-// Menu side toggle
-$('document').ready(function(){
-  $('button.navbar-toggle').click(function(){
-    var navbar_obj = $($(this).data("target"));
-    navbar_obj.toggleClass("open");
-  });
-});
---
+++ Sample Code/Others/Animated Buttons/bookmark/README.md
@@ -0,0 +1,4 @@
+# Bookmark
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/aaroniker/pen/oNbdyNb](https://codepen.io/aaroniker/pen/oNbdyNb).
+
