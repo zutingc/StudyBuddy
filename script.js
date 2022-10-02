708c7857357c4cc0ed107aa0b0b165ce78fb91a0
zchen125 <zchen125@stevens.edu>
2022-10-01 23:15:00 -0400

    added like/dislike button UI

--- script.js
+++ script.js
@@ -16,36 +16,58 @@ jQuery(function ($) {
     });
 });
 
-// for each class .btnUI i, add "href="#collapseCommenti" to .collapseBtn and id="collapseBtni" to each .collapseComments
+
 function addCollapse() {
-    for (let i = 0; i < document.getElementsByClassName("btnUI").length; i++) {
-        // iterative approach, run through each one by nodeNumber, i
+    for (let i = 0; i < document.getElementsByClassName("eachPost").length; i++) {
+        // for each class .btnUI i, add "href="#collapseCommenti" to .collapseBtn and id="collapseBtni" to each .collapseComments
         let commentBtn = document.getElementsByClassName("collapseBtn")[i]; // grab the HTML code for node of nodeNumber
         let commentContent = document.getElementsByClassName("collapseComments")[i];
-        // commentBtn.classList.add("collapseBtn" + i.toString());
         commentBtn.href = "#collapseBtn" + i.toString();
         commentContent.setAttribute("id", "collapseBtn" + i.toString());
+        // for post, add unique numLikes id
+        document.getElementsByClassName("likeCount")[i].setAttribute("id", "numLikes" + i.toString());
+        // like button actions
+        let likeElem = document.getElementsByClassName("likeBtn")[i];
+        let dislikeElem = document.getElementsByClassName("dislikeBtn")[i];
+        likeElem.addEventListener('click', function (event) { // add likes
+            var numLikes = parseInt(document.getElementById('numLikes' + i.toString()).innerHTML);
+            if (likeElem.classList.contains('liked')) { // already liked
+                likeElem.classList.remove('liked');
+                numLikes--;
+            } 
+            else if (dislikeElem.classList.contains('disliked')) { // already disliked
+                dislikeElem.classList.remove('disliked');
+                likeElem.classList.add('liked');
+                numLikes += 2;
+            } 
+            else { // no like/dislike
+                likeElem.classList.add('liked');
+                numLikes++;
+            } 
+            document.getElementById("numLikes" + i.toString()).innerHTML = numLikes;
+        });
+        dislikeElem.addEventListener('click', function (event) { // add likes
+            var numLikes = parseInt(document.getElementById('numLikes' + i.toString()).innerHTML);
+            if (dislikeElem.classList.contains('disliked')) { // already disliked
+                dislikeElem.classList.remove('disliked');
+                numLikes++;
+            } 
+            else if (likeElem.classList.contains('liked')) { // already liked
+                likeElem.classList.remove('liked');
+                dislikeElem.classList.add('disliked');
+                numLikes -= 2;
+            } 
+            else { // no like/dislike
+                dislikeElem.classList.add('disliked');
+                numLikes--;
+            } 
+            document.getElementById("numLikes" + i.toString()).innerHTML = numLikes;
+        });
     }
 }
 addCollapse();
 
-function addCounter() {
-    let elementsArray = document.querySelectorAll(".likeBtn");
-    elementsArray.forEach(function (elem) {
-    elem.addEventListener('click', function (event) {
-        if (elem.classList.contains('liked')) {
-            elem.classList.remove('liked');
-        } 
-        else if (elem.classList.contains('disliked')) {
-            elem.classList.remove('liked');
-        } 
-        else {
-            elem.classList.add('liked');
-        }
-    });
-});
-}
-
+// add event listener for activeBtn
 let elementsArray = document.querySelectorAll(".activeBtn");
 elementsArray.forEach(function (elem) {
     elem.addEventListener('click', function (event) {
